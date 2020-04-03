c86ec0b04596f7413388346717e6508f975e1512
krokoloko <daan.j.ruting@gmail.com>
2020-04-03 14:14:47 +0200

    Object can spawn
    Object also rotates with the surface and with the normal maps.

---
+++ SparkToLearnProject/Assets/Scripts/ObjectRelatedScripts/SpawnObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour
+{
+    public static void SpawnObjectOnRaycastHit(Transform orientation,GameObject spawnObject)
+    {
+        Ray ray = new Ray(orientation.position, orientation.forward);
+        RaycastHit hit;
+  
+        Debug.Log("scanning");
+        if (Physics.Raycast(ray, out hit))
+        {
+            Quaternion hitRotation = Quaternion.Euler(hit.normal.x,hit.normal.y,hit.normal.z)*hit.collider.transform.rotation;
+            Instantiate<GameObject>(spawnObject, hit.point, hitRotation);
+        }
+    }
+}
---
+++ SparkToLearnProject/Assets/Scripts/ObjectRelatedScripts/TestObjectScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestObjectScript : MonoBehaviour
+{
+    [SerializeField] private KeyCode _testKey = KeyCode.Space;
+    [SerializeField] private GameObject _testObject;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(_testKey))
+        {
+            Debug.Log("hello");
+            SpawnObject.SpawnObjectOnRaycastHit(transform, _testObject);
+        }
+    }
+}
