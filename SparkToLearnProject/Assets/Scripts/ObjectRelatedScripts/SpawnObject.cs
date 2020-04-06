b1237e81529ec836424c9d26381680340f10f6b2
krokoloko <daan.j.ruting@gmail.com>
2020-04-06 13:24:59 +0200

    Created placement preview
    You can now access a coroutine which will allow you to see the preview of the object placement. 2 Bugs have been fixed while working on this feature.1: The object would hit itself while previewing the placement 2: The object would spawn at the 0,0,0 coordinates when starting the preview.

--- SparkToLearnProject/Assets/Scripts/ObjectRelatedScripts/SpawnObject.cs
+++ SparkToLearnProject/Assets/Scripts/ObjectRelatedScripts/SpawnObject.cs
@@ -1,19 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
 public class SpawnObject : MonoBehaviour
 {
-    public static void SpawnObjectOnRaycastHit(Transform orientation,GameObject spawnObject)
+    public static void SpawnObjectOnRaycastHit(Transform orientation,GameObject spawnObject,LayerMask mask)
     {
         Ray ray = new Ray(orientation.position, orientation.forward);
         RaycastHit hit;
   
-        Debug.Log("scanning");
-        if (Physics.Raycast(ray, out hit))
+        if (Physics.Raycast(ray, out hit,100,mask))
         {
-            Quaternion hitRotation = Quaternion.Euler(hit.normal.x,hit.normal.y,hit.normal.z)*hit.collider.transform.rotation;
+            
+            Quaternion hitRotation = Quaternion.Euler(hit.normal.x,hit.normal.y,hit.normal.z) * hit.collider.transform.rotation;
             Instantiate<GameObject>(spawnObject, hit.point, hitRotation);
         }
+    }
+    public static IEnumerator HighlightObjectOnRaycastHit(GameObject orientation, GameObject spawnObject, Func<bool> breakCondition, LayerMask mask)
+    {
+        Quaternion hitRotation;
+        GameObject highlight = null;
+
+        yield return new WaitForEndOfFrame();
+
+        while (true)
+        {
+            RaycastHit hit;
+            Ray ray = new Ray(orientation.transform.position, orientation.transform.forward);
+
+            if (Physics.Raycast(ray, out hit,100,mask))
+            {
+                highlight = (highlight == null) ? Instantiate<GameObject>(spawnObject) : highlight;
+
+                hitRotation = Quaternion.Euler(hit.normal.x, hit.normal.y, hit.normal.z) * hit.collider.transform.rotation;
+
+                highlight.transform.SetPositionAndRotation(hit.point, hitRotation);
+            }
+
+            yield return new WaitForEndOfFrame();
+
+            if (breakCondition())
+            {
+                break;
+            }
+        }
+
+        Destroy(highlight);
     }
 }
--- SparkToLearnProject/Assets/Scripts/ObjectRelatedScripts/TestObjectScript.cs
+++ SparkToLearnProject/Assets/Scripts/ObjectRelatedScripts/TestObjectScript.cs
@@ -6,14 +6,32 @@ public class TestObjectScript : MonoBehaviour
 {
     [SerializeField] private KeyCode _testKey = KeyCode.Space;
     [SerializeField] private GameObject _testObject;
+    [SerializeField] private LayerMask _mask;
+
+    private bool _readyToPlace = false;
+    private bool _activateSpawn = false;
+
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(_testKey))
+        _activateSpawn = Input.GetKeyDown(_testKey);
+        if (_activateSpawn && !_readyToPlace)
         {
-            Debug.Log("hello");
-            SpawnObject.SpawnObjectOnRaycastHit(transform, _testObject);
+            Debug.Log("Highlight");
+            _readyToPlace = true;
+            StartCoroutine(SpawnObject.HighlightObjectOnRaycastHit(gameObject, _testObject,BreakCondition,_mask));
         }
+    }
+    private bool BreakCondition()
+    {
+        if (_readyToPlace && _activateSpawn)
+        {
+            Debug.Log("Break");
+            SpawnObject.SpawnObjectOnRaycastHit(transform, _testObject, _mask);
+            _readyToPlace = false;
+            return true;
+        }
+        else return false;
     }
 }
