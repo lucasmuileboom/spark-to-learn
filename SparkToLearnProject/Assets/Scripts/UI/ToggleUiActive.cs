a731aaa35dc8161b6ce5119455432af5359d9248
lucasmuileboom <muileboomlucas@gmail.com>
2020-04-16 15:35:07 +0100

    Linked the menu with the ui

---
+++ SparkToLearnProject/Assets/Scripts/UI/ToggleUiActive.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleUiActive : MonoBehaviour
+{
+    [SerializeField] private GameObject _uiObject;
+
+    public void ToggleUi()
+    {
+        _uiObject.SetActive(!_uiObject.activeSelf);
+    }
+    public void SetUi(bool newActive)
+    {
+        _uiObject.SetActive(newActive);
+    }
+}
