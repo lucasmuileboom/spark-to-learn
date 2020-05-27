b50dadb751f92d93c4ba9f6a995e850e3b09ec59
krokoloko <daan.j.ruting@gmail.com>
2020-05-27 15:14:23 +0200

    Created base codestructure
    Waiting for the other tutorial UI assets.

---
+++ SparkToLearnProject/Assets/Scripts/Tutorial/TutorialSegment.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialSegment : MonoBehaviour
+{
+    [SerializeField] private Button _nextSegmentButton;
+    public Button NextSegmentButton
+    {
+        get { return _nextSegmentButton; }
+        private set { _nextSegmentButton = NextSegmentButton; } 
+    }
+
+    [SerializeField] private Button _previousSegmentButton;
+    public Button PreviousSegmentButton
+    {
+        get { return _previousSegmentButton; }
+        private set { _previousSegmentButton = PreviousSegmentButton; }
+    }
+
+    [SerializeField] private Button _stopSequanceButton;
+    public Button StopSequenceButton
+    {
+        get { return _stopSequanceButton; }
+        private set { _stopSequanceButton = StopSequenceButton; }
+    }
+}
---
+++ SparkToLearnProject/Assets/Scripts/Tutorial/TutorialSequence.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class TutorialSequence : MonoBehaviour
+{
+    [SerializeField]
+    private List<TutorialSegment> _tutorialPages;
+
+    [SerializeField]
+    private bool _onlyOnce = true;
+
+    [SerializeField]
+    private UnityEvent _onStart;
+
+    [SerializeField]
+    private UnityEvent<int> _onNext;
+
+    [SerializeField]
+    private UnityEvent<int> _onPrevious;
+
+    [SerializeField]
+    private UnityEvent _onEnd;
+
+
+    private void Awake()
+    {
+        for (int i = 0; i < _tutorialPages.Count; i++) {
+            _tutorialPages[i].NextSegmentButton?.onClick.AddListener(() => { _onNext?.Invoke(i); });
+            _tutorialPages[i].PreviousSegmentButton?.onClick.AddListener(() => { _onPrevious?.Invoke(i); });
+            _tutorialPages[i].StopSequenceButton?.onClick.AddListener(() => { _onEnd?.Invoke(); });
+            _tutorialPages[i].enabled = false;
+        }
+    }
+
+    public void StartSequence()
+    {
+        _tutorialPages[0].enabled = true;
+        _onStart?.Invoke();
+    }
+
+    public void NextSegment(int segment)
+    {
+        _tutorialPages[Mathf.Min(segment + 1,_tutorialPages.Count-1)].enabled = true;
+        _tutorialPages[segment].enabled = false;
+    }
+    public void PreviousSegment(int segment)
+    {
+        _tutorialPages[Mathf.Max(segment - 1,0)].enabled = true;
+        _tutorialPages[segment].enabled = false;
+    }
+    public void StopSequence()
+    {
+        foreach(TutorialSegment segment in _tutorialPages)
+        {
+            segment.enabled = false;
+        }
+    }
+}
